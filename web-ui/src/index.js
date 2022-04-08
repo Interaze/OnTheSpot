20c7fb598d7de62214a4d86357867c5fb4a7ba1c
AJ Wilkinson <ajwilkinson18@gmail.com>
2022-04-08 13:13:55 -0500

    Nothin

--- web-ui/src/index.js
+++ web-ui/src/index.js
@@ -1,12 +1,13 @@
-import React from "react";
+
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "./index.css";
 ReactDOM.render(
-  <BrowserRouter>
+  <StrictMode>
     <App />
-  </BrowserRouter>,
+  </StrictMode>,
   document.getElementById("root")
 );
 
