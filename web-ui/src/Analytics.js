0225157566837ba88749a3fcc43b733f5b97ceed
AJ Wilkinson <ajwilkinson18@gmail.com>
2022-04-14 14:57:54 -0500

    Began other pages. Login page is problem.

--- web-ui/src/Analytics.js
+++ web-ui/src/Analytics.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import {BiArrowBack} from 'react-icons/bi'
+import {Link} from 'react-router-dom';
 
 function Analytics() {
   return (
-    <div>Analytics</div>
+    <div className="landing-page">
+      <div className="verticle-menu">
+        <a href="#" className="active">
+          Task 1
+        </a>
+        <a href="#">Task 2</a>
+        <a href="#">Task 3</a>
+        <a href="#">Task 4</a>
+        <a href="#">Task 5</a>
+      </div>
+      <Link to="/" className="settings-button"><BiArrowBack color='white' fontSize='1.5rem'/></Link>
+    </div>
   )
 }
 
--- web-ui/src/Settings.js
+++ web-ui/src/Settings.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import {BiArrowBack} from 'react-icons/bi'
+import {Link} from 'react-router-dom';
 
 function Settings() {
   return (
-    <div>Settings</div>
+    <div className="landing-page">
+      <Link to="/" className="settings-button"><BiArrowBack color='white' fontSize='1.5rem'/></Link>
+    </div>
   )
 }
 
